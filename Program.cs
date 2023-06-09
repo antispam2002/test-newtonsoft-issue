2a4443df7d02f3f52c8ba6982f32033ef734ac28
Aleksey Tarkhov <aleksey.tarkhov@paessler.com>
2023-06-09 16:22:36 +0200

    chore: set runtime to 1 second

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ var timer2 = new Timer(state =>
   TimeSpan.FromMilliseconds(1),
   TimeSpan.FromMilliseconds(1));
 // sleep in main thread for a while
-Thread.Sleep(200);
+Thread.Sleep(1000);
 
 public class Payload
 {
